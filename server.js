8fece89af843418da595e633f0ba1f1b1828cc1f
FajsiEx <fajsiexofficial@gmail.com>
2019-07-09 12:45:45 +0200

    + cors 4

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ app.use(cors({
     origin: '*',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 }));
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 require("./routes").init(app);
 
 app.listen(process.env.PORT || 3211, function () {
