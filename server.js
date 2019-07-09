6055b7a811d02f322961203a3ae7c7e297b70776
FajsiEx <fajsiexofficial@gmail.com>
2019-07-09 12:42:57 +0200

    + cors attempt 3

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ const app = express();
 
 const cors = require("cors");
 
-app.use(cors());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
+app.use(cors({
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}));
 require("./routes").init(app);
 
 app.listen(process.env.PORT || 3211, function () {
