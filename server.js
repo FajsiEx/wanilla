eb35d291c7b1a70948489eb8f16eddcfdf654c59
FajsiEx <fajsiexofficial@gmail.com>
2019-07-09 12:37:31 +0200

    + cors

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+
+app.use(cors());
+
 require("./routes").init(app);
 
 app.listen(process.env.PORT || 3211, function () {
