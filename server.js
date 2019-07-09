4d93017f855bf78bc7c9a0d2937be2f4440926c6
FajsiEx <fajsiexofficial@gmail.com>
2019-07-09 12:40:10 +0200

    + moar cors

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 require("./routes").init(app);
 
